10c2a2dd2218ebfedc02e906fde3433bf49a3a6f
JakeTSherman <jake.sherman@kellogg.northwestern.edu>
2022-01-25 05:49:35 +0000

    domain model attempt 1

--- crm/crm.sql
+++ crm/crm.sql
@@ -2,4 +2,39 @@
 -- run of this script
 -- e.g. DROP TABLE IF EXISTS ______;
 
--- CREATE TABLES
+DROP TABLE IF EXISTS contacts;
+DROP TABLE IF EXISTS activities;
+DROP TABLE IF EXISTS companies;
+DROP TABLE IF EXISTS users;    
+
+
+
+CREATE TABLE contacts (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    first_name TEXT,
+    last_name TEXT,
+    company_id TEXT,
+    email TEXT,
+    phone_number TEXT
+);
+
+CREATE TABLE activities (
+    id INTEGER PRIMARY KEY AUTOINCREMENT, 
+    contact_id INTEGER, 
+    user_id INTEGER,
+    date TEXT,
+    description TEXT
+);
+
+CREATE TABLE companies (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    industry TEXT
+);
+
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    first_name TEXT,
+    last_name TEXT,
+    email TEXT
+);
